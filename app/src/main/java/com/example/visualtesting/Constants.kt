32fc5a77fa8be496ad9bb5cfca504f3151a9e7b8
deepthi-mathi <43693602+deepthi-mathi@users.noreply.github.com>
2025-03-24 11:47:06 +0530

    Update Constants.kt

--- app/src/main/java/com/example/visualtesting/Constants.kt
+++ app/src/main/java/com/example/visualtesting/Constants.kt
@@ -1,7 +1,7 @@
 package com.example.visualtesting
 
 object Constants {
-    const val API_KEY = "009e231e77fa44529c60de2c5cc092b6"
+    const val API_KEY = ""
     const val BASE_URL = "https://backbase-open-ai-chat-gpt4.openai.azure.com"
     const val DEPLOYMENT_ID = "gpt-4o"
     const val DEPLOYMENT_VERSION = "2024-02-01"
